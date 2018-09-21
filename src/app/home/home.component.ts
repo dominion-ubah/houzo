f2f22304fb66b69432a1a73a863c945dab75b294
ayomide osunkojo <domee.ubah@gmail.com>
2018-09-21 03:57:33 +0100

    with gx scroll service added itegrated to ui

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HouzlistService } from '../shared/houzlist.service';
+import { forEach } from '@angular/router/src/utils/collection';
 
 declare var Chance: any; // for externals librairies
 @Component({
@@ -10,66 +11,66 @@ declare var Chance: any; // for externals librairies
 export class HomeComponent implements OnInit {
   data = []
   array = [];
-  sum = 8;
-  throttle = 300;
+  adder = 6;
+  initVal:number;
+  throttle = 100;
   scrollDistance = 1;
-  scrollUpDistance = 2;
+  // scrollUpDistance = 2;
   direction = '';
-  chance: any;
+  // chance: any;
 
   constructor(private _houzService:HouzlistService) {
-    this.chance  = new Chance();
-    this.appendItems(0, this.sum);
+    // this.chance  = new Chance();
+    // this.appendItems(0, this.sum);
+    this.determI();
   }
 
 
   ngOnInit() {
     this._houzService.getHouzData().subscribe(data => {
-      this.data = data.slice(0, 240);
-      console.log('list', this.data);
+      this.data = data.slice(0, 20);
+      console.log('list', this.array);
+      
     },
     error => {
       console.error(error);
     },
     ()=> {
       console.log('doe');
+      this.initVal = 6;
+      if(this.array.length == 0) {
+        this.determI();
+        console.log("ran it", this.initVal, this.adder)
+      }
     })
   }
-  
-  addItems(startIndex, endIndex, _method) {
-    for (let i = 0; i < this.sum; ++i) {
-      this.array[_method]([i, ' ', this.generateWord()].join(''));
+
+determI(){
+  let finalVal = this.initVal + this.adder;
+  this.popul(finalVal, this.data);
+  this.initVal = finalVal; 
+
+}
+popul(finalVal, arr){
+  arr.forEach((e, i) => {
+    if(i <= finalVal){
+      if(finalVal < arr.length) {
+        this.array.push(e);
+        console.log('fi', finalVal)
+        return this.array;
+      }      
     }
-  }
-  
-  appendItems(startIndex, endIndex) {
-    this.addItems(startIndex, endIndex, 'push');
-  }
-  
-  prependItems(startIndex, endIndex) {
-    this.addItems(startIndex, endIndex, 'unshift');
+  })
   }
 
   onScrollDown (ev) {
     console.log('scrolled down!!', ev);
-
-    // add another 20 items
-    const start = this.sum;
-    this.sum += 20;
-    this.appendItems(start, this.sum);
-    
+    this.determI();    
     this.direction = 'down'
   }
   
-  onUp(ev) {
-    console.log('scrolled up!', ev);
-    const start = this.sum;
-    this.sum += 20;
-    this.prependItems(start, this.sum);
+  // onUp(ev) {
+  //   this.direction = 'up';
+  // }
   
-    this.direction = 'up';
-  }
-  generateWord() {
-    return this.chance.word();
-  }
 }
